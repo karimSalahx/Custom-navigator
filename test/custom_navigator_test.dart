f15379f55da4d6c87675f0e1ec67001485543907
karem <karemmohamed2002@gmail.com>
2021-03-24 14:55:08 +0000

    c

--- test/custom_navigator_test.dart
+++ test/custom_navigator_test.dart
@@ -1,8 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:custom_navigator/custom_navigator.dart';
-
 void main() {
-  test('adds one to input values', () {
-  });
+  test('adds one to input values', () {});
 }
